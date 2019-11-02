4383510a224b63c8a3adc82c266837915f76a656
Ruggero Nocera <9341160+rogierz@users.noreply.github.com>
2019-11-02 13:20:47 +0100

    Added Lab04\es02.c

---
+++ Lab04/es02.c
@@ -0,0 +1,39 @@
+//
+// Created by Ruggero Nocera on 02/11/2019.
+//
+
+#include <stdio.h>
+
+int majority(int *a, int N);
+int majorityR(int *a, int l, int r);
+
+int main(){
+    int vett[8] = {0, 1, 0, 2, 3, 4, 0, 5};
+    printf("%d", majority(vett, 8));
+    return 0;
+}
+int majority(int *a, int N){
+    return majorityR(a, 0, N-1);
+}
+int majorityR(int *a, int l, int r){
+    int m = (r+l)/2;
+    int sx, dx, i;
+    int occSx, occDx;
+    occSx = occDx = 0;
+    if(l == r) return a[l];
+    if(r-l == 1){
+        if(a[l] == a[r]) return a[l];
+        return -1;
+    }
+    sx = majorityR(a, l, m);
+    dx = majorityR(a, m+1, r);
+    if(sx == -1 && dx == -1) return -1;
+    for(i = l; i <= r; i++){
+        if(a[i] == sx) occSx++;
+        if(a[i] == dx) occDx++;
+    }
+    if(occSx >= occDx && occSx > (r-l+1)/2) return sx;
+    if(occDx > (r-l+1)/2) return dx;
+    return -1;
+
+}
