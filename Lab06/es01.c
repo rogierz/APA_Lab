87eef9f0ba91c880e0d59b18bb7d8b386ae68c28
Ruggero Nocera <9341160+rogierz@users.noreply.github.com>
2019-11-16 11:21:28 +0100

    Reduced complexity to O(N) (From O(N^2))

--- Lab06/es01.c
+++ Lab06/es01.c
@@ -8,9 +8,12 @@ typedef struct{
 } edge;
 
 void stampaVertexCover(int *nodes, int nodesLen, edge * edges, int edgesLen);
-void powerset(int pos, int *val, int len, int start, int* sol, edge *edges, int edgesLen);
-void validate(int *sol, int len, edge *edges, int edgesLen);
 
+//void generateDeT(int pos, int *val, int len, int start, int* sol, edge *edges, int edgesLen);
+void generateDR(int pos, int* val, int len, int* sol, edge *edges, int edgesLen);
+
+void validateDeT(int *sol, int len, edge *edges, int edgesLen); //O(N^2)
+void validateDR(int *sol, int len, edge *edges, int edgesLen); //O(N)
 int main(){
     int *nodes;
     edge *edges;
@@ -33,24 +36,56 @@ int main(){
 }
 
 void stampaVertexCover(int *nodes, int nodesLen, edge * edges, int edgesLen){
-    int* sol = malloc(nodesLen* sizeof(int));
-    powerset(0, nodes, nodesLen, 0, sol, edges, edgesLen);
+    int* sol = calloc(nodesLen, sizeof(int));
+    //generateDeT(0, nodes, nodesLen, 0, sol, edges, edgesLen);
+    generateDR(0, nodes, nodesLen, sol, edges, edgesLen);
 }
 
-void powerset(int pos, int *val, int len, int start, int *sol, edge *edges, int edgesLen){
+void generateDR(int pos, int *val, int len, int *sol, edge *edges, int edgesLen){
+
+    if(pos >= len){
+        validateDR(sol, len, edges, edgesLen);
+        return;
+    }
+    sol[pos] = 0;
+    generateDR(pos+1, val, len, sol, edges, edgesLen);
+    sol[pos] = 1;
+    generateDR(pos+1, val, len, sol, edges, edgesLen);
+}
+
+void validateDR(int *sol, int len, edge *edges, int edgesLen){
+    int i;
+    int flag = 1;
+    int firstPrinted = 0;
+    for(i = 0; i < edgesLen; i++){
+        if(sol[edges[i].u] == 0 && sol[edges[i].v] == 0) flag = 0;
+    }
+    if(flag) {
+        printf("(");
+        for (i = 0; i < len; i++) {
+            if(sol[i] == 1){
+                if (firstPrinted == 1) printf(", ");
+                printf("%d", i);
+                firstPrinted = 1;
+            }
+        }
+        printf(")\n");
+    }
+}
+/*void generateDeT(int pos, int *val, int len, int start, int *sol, edge *edges, int edgesLen){
     int i;
     if(start >= len){
-        if(pos > 0) validate(sol, pos, edges, edgesLen);
+        if(pos > 0) validateDeT(sol, pos, edges, edgesLen);
         return;
     }
     for(i = start; i < len; i++){
         sol[pos] = val[i];
-        powerset(pos+1, val, len, i+1, sol, edges, edgesLen);
+        generateDeT(pos+1, val, len, i+1, sol, edges, edgesLen);
     }
-    powerset(pos, val, len, len, sol, edges, edgesLen);
-}
+    generateDeT(pos, val, len, len, sol, edges, edgesLen);
+}*/
 
-void validate(int *sol, int len, edge *edges, int edgesLen){
+void validateDeT(int *sol, int len, edge *edges, int edgesLen){
     int i, j;
     int mark[edgesLen];
     int flag = 1;
